ab5cca963ad8fca7c4ba9bce07159c54980627ba
jadeye <judmojo@gmail.com>
2022-08-03 23:41:09 +0300

    Update UserFinder.js
    In your course on Udemy I downloaded this file in class:
     167. The Component Lifecycle (Class-based Components Only!
    
    It's mising the `DUMMY_USERS const ` and `Fragment` doesn't render the className thus leaving the `search input` element out side the frame...

--- extra-files/UserFinder.js
+++ extra-files/UserFinder.js
@@ -1,6 +1,12 @@
 import { Fragment, useState, useEffect } from 'react';
+import classes from "./UserFinder.module.css";
+import Users from "./Users";
 
-import Users from './Users';
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
 
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
@@ -17,10 +23,10 @@ const UserFinder = () => {
   };
 
   return (
-    <Fragment>
+     <div className={classes.finder}>
       <input type='search' onChange={searchChangeHandler} />
       <Users users={filteredUsers} />
-    </Fragment>
+    </div>
   );
 };
 
